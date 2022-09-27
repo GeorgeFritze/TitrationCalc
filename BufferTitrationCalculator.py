d4770030a63675944ade01ae87d1394a1aa0d2ff
George Fritze George <gwf8@pitt.edu>
2022-09-27 13:01:53 -0400

    weak acid work

--- BufferTitrationCalculator.py
+++ BufferTitrationCalculator.py
@@ -14,9 +14,6 @@ if TypeofSample=="strong":
 
     MolesofTitrant=VolumeofTitrant*MolarityofTitrant
 
-
-
-if TypeofSample == "strong":
     Question1=input("Is your sample an acid or a base?")
     FinalMoles=abs(MolesofSample-MolesofTitrant)
     FinalVolume=VolumeofSample+VolumeofTitrant
@@ -35,47 +32,71 @@ if TypeofSample == "strong":
                 print("Congratulations! You just completed a titration. The pH of your solution is", float(-1*np.log(FinalMolarity)))
 
 
-
-
 if TypeofSample == "weak":
+    Question2=input("Is your sample an acid or a base?")
     VolumeofSample=float(input("What is the volume of your sample in Liters?"))
     MolarityofSample=float(input("What is the molarity of your sample?"))
-    SampleKA=float(input("What is the K value of your sample?"))
+    SampleK=float(input("What is the K value of your sample?"))
     VolumeofTitrant=float(input("How much Titrant are you adding in Liters?"))
     MolarityofTitrant=float(input("What is the molarity of your Titrant?"))
 
-    
-    acidconcentration=((SampleKA)-np.sqrt((SampleKA**2)-(4*(-1)*(MolarityofSample*SampleKA))))/(-2)
-    pH=(-1)*np.log10(acidconcentration)
-    print("The pH of your sample is originally", pH)
+    if Question2 == "acid":
 
-    MolesofSample=VolumeofSample*MolarityofSample
+        acidconcentration=((SampleK)-np.sqrt((SampleK**2)-(4*(-1)*(MolarityofSample*SampleK))))/(-2)
+        pH=(-1)*np.log10(acidconcentration)
+        print("The pH of your sample is originally", pH)
 
-    MolesofTitrant=VolumeofTitrant*MolarityofTitrant
+        MolesofSample=VolumeofSample*MolarityofSample
 
-    if MolesofSample > MolesofTitrant:
-        FinalMoles=MolesofSample-MolesofTitrant
-        FinalVolume=VolumeofSample+VolumeofTitrant
-        FinalMolarity=FinalMoles/FinalVolume
+        MolesofTitrant=VolumeofTitrant*MolarityofTitrant
 
-        if .11<MolesofTitrant/FinalMoles<10.01:
+        if MolesofSample > MolesofTitrant:
+            FinalMoles=MolesofSample-MolesofTitrant
+            FinalVolume=VolumeofSample+VolumeofTitrant
+            FinalMolarity=FinalMoles/FinalVolume
 
-            pHWhenFinished=float((-1)*np.log(SampleKA)+np.log((MolesofTitrant/FinalVolume)/(FinalMolarity)))
+            if .009<MolesofTitrant/FinalMoles<100.01:
 
-            print("Congratulations!, the molarity of your existing sample is", round(FinalMolarity, 2), "and the pH is", round(pHWhenFinished, 2))
+                pHWhenFinished=float((-1)*np.log10(SampleK)+np.log10((MolesofTitrant/FinalVolume)/(FinalMolarity)))
 
-        newacidconcentration=((SampleKA)-np.sqrt((SampleKA**2)-(4*(-1)*(FinalMolarity*SampleKA))))/(-2)
-        finalpH=(-1)*np.log10(newacidconcentration)
-        print("Congratulations!, the molarity of your existing sample is", round(FinalMolarity, 2), "and the pH is", round(finalpH, 2))
+                print("Congratulations!, the molarity of your existing sample is", round(FinalMolarity, 2), "and the pH is", round(pHWhenFinished, 2))
 
-    if MolesofSample < MolesofTitrant:
-        FinalMoles=MolesofTitrant-MolesofSample
-        FinalVolume=VolumeofSample+VolumeofTitrant
-        FinalMolarity=FinalMoles/FinalVolume
-        pHWhenFinished=float((-1)*np.log(FinalMolarity))
+            else:
+                newacidconcentration=((SampleK)-np.sqrt((SampleK**2)-(4*(-1)*(FinalMolarity*SampleK))))/(-2)
+                finalpH=(-1)*np.log10(newacidconcentration)
+                print("Congratulations!, the molarity of your existing sample is", round(FinalMolarity, 2), "and the pH is", round(finalpH, 2))
 
-        print("Congratulations!, you went past the equivalence point and the pH of your sample is", round(pHWhenFinished, 2))
+        if MolesofSample < MolesofTitrant:
+            FinalMoles=MolesofTitrant-MolesofSample
+            FinalVolume=VolumeofSample+VolumeofTitrant
+            FinalMolarity=FinalMoles/FinalVolume
+            pHWhenFinished=float((-1)*np.log(FinalMolarity))
 
+            print("Congratulations!, you went past the equivalence point and the pH of your sample is", round(pHWhenFinished, 2))
 
+    if Question2 == "base":
+        if MolesofSample > MolesofTitrant:
+            FinalMoles=MolesofSample-MolesofTitrant
+            FinalVolume=VolumeofSample+VolumeofTitrant
+            FinalMolarity=FinalMoles/FinalVolume
+
+            if .009<MolesofTitrant/FinalMoles<100.01:
+
+                pOHWhenFinished=float((-1)*np.log10(SampleK)+np.log10((MolesofTitrant/FinalVolume)/(FinalMolarity)))
+
+                print("Congratulations!, the molarity of your existing sample is", round(FinalMolarity, 2), "and the pH is", round(14-pOHWhenFinished, 2))
+
+            else:
+                newacidconcentration=((SampleK)-np.sqrt((SampleK**2)-(4*(-1)*(FinalMolarity*SampleK))))/(-2)
+                finalpH=(-1)*np.log10(newacidconcentration)
+                print("Congratulations!, the molarity of your existing sample is", round(FinalMolarity, 2), "and the pH is", round(finalpH, 2))
+
+        if MolesofSample < MolesofTitrant:
+            FinalMoles=MolesofTitrant-MolesofSample
+            FinalVolume=VolumeofSample+VolumeofTitrant
+            FinalMolarity=FinalMoles/FinalVolume
+            pHWhenFinished=float((-1)*np.log(FinalMolarity))
+
+            print("Congratulations!, you went past the equivalence point and the pH of your sample is", round(pHWhenFinished, 2))
 
 
